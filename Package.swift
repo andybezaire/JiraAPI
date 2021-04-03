b18428bdf31194ebffce24cd4c1b9a19bd264eab
andy <andy.bezaire@gmail.com>
2021-04-03 12:30:15 +0300

    cleaning up package file

--- Package.swift
+++ Package.swift
@@ -6,12 +6,11 @@ let package = Package(
     name: "JiraAPI",
     platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
-        .library(name: "JiraAPI", targets: ["JiraAPI"]),
+        .library(name: "JiraAPI", targets: ["JiraAPI"])
     ],
-    dependencies: [
-    ],
+    dependencies: [],
     targets: [
         .target(name: "JiraAPI", dependencies: []),
-        .testTarget(name: "JiraAPITests", dependencies: ["JiraAPI"]),
+        .testTarget(name: "JiraAPITests", dependencies: ["JiraAPI"])
     ]
 )
